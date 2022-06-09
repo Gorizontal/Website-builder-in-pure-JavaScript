d2516b2f77e970b45c6db4155db585cfa5bc4ce8
Alex <necrasoff.alex@yandex.ru>
2022-06-10 03:37:33 +0500

    optimizaded

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const model = [
     {type: 'columns', value: [
         '1111111',
         '2222222',
+        '3333333',
+        '2222222',
         '3333333'
-    ]}
+    ]},
+    {type:'image', value: './assets/image (1).png'}
 ]
 
 
@@ -14,24 +17,58 @@ const $site = document.querySelector('#site');
 model.forEach( block => {
     let html = '';
     if(block.type === 'title') {
-        html = `
-        <div class="row">
-            <div class="col-sm">
-                <h1>${block.value}</h1>
-            </div>
-        </div>`
+        html = title(block);
     } else if (block.type == 'text') {
-        html = `
-        <div class="row">
-            <div class="col-sm">
-                <p>${block.value}</p>
-            </div>
-        </div>
-        `
+        html = text(block);
     } else if (block.type == 'columns') {
-
+        html = columns(block);
+    } else if (block.type == 'image') {
+        html = image(block);
     }
 
     $site.insertAdjacentHTML('beforeend', html)
     
 })
+
+
+function title (block) {
+    return `
+    <div class="row">
+        <div class="col-sm">
+            <h1>${block.value}</h1>
+        </div>
+    </div>`
+}
+
+function text (block) {
+    return `
+    <div class="row">
+        <div class="col-sm">
+            <p>${block.value}</p>
+        </div>
+    </div>
+    `
+}
+
+function columns (block) {
+
+    const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
+    return `
+    <div class="row">
+            ${html.join('')}
+        </div>
+        `
+}
+
+
+
+
+
+
+function image (block) {
+    return `
+    <div class="row">
+        <img src ='${block.value}'>
+    </div>
+    `
+}
